d4c769ef530a83dd74eeaa4bb80ce908d5990366
Abhishek Pandey <98042094+abhishek2813@users.noreply.github.com>
2023-01-12 22:44:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function threeSum(arr, target) {
   //your code here
+	let sum = 0;
+	for (let index of arr) {
+		sum +=arr[index] 
+		if(sum ==target){
+			return sum;
+		}
+	}
 }
 
 module.exports = threeSum;
